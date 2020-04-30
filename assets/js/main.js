58e575c93125941200b813b96d217b81669b617e
MarcelGF <marcelgarciafranca@gmail.com>
2020-04-30 09:06:22 -0300

    workd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,36 @@
 let consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z']
 let vowels = ['a', 'e', 'i', 'o', 'u' ]
 
-let formattedWord = ''
+let formattedWord = []
 
 function audioPlay(idAudio) {
 	let audio = new Audio('assets/audio/' + idAudio + '-apolo.mp3')
-	formattedWord += idAudio
+
 	audio.play()
 
+	formattingWord(idAudio)
+
 	console.log('idAudio ' + idAudio)
+}
+
+function formattingWord(id) {
+	formattedWord.push(id)
+	let reducedArray = (accumulator, currentValue) => accumulator + currentValue
+	let finalWord = formattedWord.reduce(reducedArray)
+	console.log('finalWord ' + finalWord)
 	console.log('formattedWord ' + formattedWord)
+
+
+	words.forEach( function(key) {
+		if(key == finalWord) {
+			console.log('workd')
+		} else {
+			// console.log('key ' + key)
+			// console.log('reducedArray ' + reducedArray)
+			// console.log('key type ' + typeof key)
+			// console.log('key reducedArray ' + typeof reducedArray)
+		}
+	})
 }
 
 function htmlRender() {
