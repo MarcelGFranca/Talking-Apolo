1eee4c32087b4f452375194fb0ecafaef88e218d
MarcelGF <marcelgarciafranca@gmail.com>
2020-04-30 08:15:26 -0300

    added buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 let consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'z']
 let vowels = ['a', 'e', 'i', 'o', 'u' ]
 
+let formattedWord = ''
+
 function audioPlay(idAudio) {
 	let audio = new Audio('assets/audio/' + idAudio + '-apolo.mp3')
-	console.log('idAudio ' + idAudio)
+	formattedWord += idAudio
 	audio.play()
+
+	console.log('idAudio ' + idAudio)
+	console.log('formattedWord ' + formattedWord)
 }
 
 function htmlRender() {
