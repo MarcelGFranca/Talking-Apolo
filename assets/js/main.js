c254167e1b8862428388547389a2028d1f13ecc1
MarcelGF <marcelgarciafranca@gmail.com>
2020-05-04 01:16:51 -0300

    removed unused code

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,6 @@ let formattedWord = []
 let reducedArray
 let finalWord
 
-// let timeout = null                                      
-
-// document.addEventListener('click', function(e) {                                      
-// 	clearTimeout(timeout)    
-// 	timeout = setTimeout(function() {                                      
-// 		formattedWord = []                                      
-// 	}, 10000)                                      
-// })   
-
 function clearFormattedWord() {
 	formattedWord = [];
 	renderHeader(null)
@@ -52,29 +43,6 @@ function formattingWord(id) {
 	finalWord = formattedWord.reduce(reducedArray)
 	console.log('finalWord ' + finalWord)
 	console.log('formattedWord ' + formattedWord)
-
-	// words.forEach( function(key) {
-	// 	if(key == finalWord) {
-
-	// 		axios.get('https://pixabay.com/api/?key=16309043-af699687042223e3c6eb97108&q=' + finalWord + '&image_type=photo&pretty=true&safesearch=true&image_type=photo&lang=pt')
-	// 			.then(function (response) {
-	// 				console.log(response.data.hits[0].webformatURL)
-	// 				finalImg = response.data.hits[0].webformatURL
-	// 			})
-	// 			.then(function (error) {
-	// 				console.log(error)
-	// 			})
-	// 			.then(function () {
-	// 				Swal.fire({
-	// 					imageUrl: finalImg,
-	// 					title: 'A palavra é ',
-	// 					text: finalWord,
-	// 					timer: 4000
-	// 				})
-	// 			})
-	// 		formattedWord = []
-	// 	}
-	// })
 }
 
 window.onload = function() {
